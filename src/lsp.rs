4defdd053ce916b96e2aa4eca0f313b3cf903203
Daniel Hauge <Animcuil@gmail.com>
2024-07-11 16:39:03 +0200

    feat: Added pub interface and tests for lsp

--- src/lsp.rs
+++ src/lsp.rs
@@ -52,10 +52,12 @@ pub mod lsp {
         fn compute_lsp_context(&mut self, doc_content: &str, parse_results: &ParseResult) {
             self.compute_symbols(parse_results);
             for rule in &parse_results.syntax.rules {
-                self.definitions.insert(
-                    rule.name.to_string(),
-                    self.location_at_offset(rule.span.start),
-                );
+                let loc = self.location_at_offset(rule.span.start);
+                self.definitions.insert(rule.name.to_string(), loc.clone());
+                self.references
+                    .entry(rule.name.to_string())
+                    .or_insert_with(Vec::new)
+                    .push(loc);
                 let hover_span = rule.span.start..rule.span.end;
                 let hover_text = doc_content[hover_span].to_string();
                 self.hover.insert(rule.name.to_string(), hover_text);
@@ -133,16 +135,86 @@ pub mod lsp {
             }
         }
 
-        fn offset_at_location(&self, location: Location) -> usize {
+        fn offset_at_location(&self, location: &Location) -> usize {
             let line_number = location.line;
             let line_offset = self.line_to_offset.get(&line_number).unwrap_or(&0);
             line_offset + location.col
         }
+
+        pub fn hover(&self, location: &Location) -> Option<&str> {
+            let offset = self.offset_at_location(location);
+            let symbol = self.symbols.get(&offset)?;
+            self.hover.get(symbol.as_str()).map(|s| s.as_str())
+        }
+
+        pub fn references(&self, location: &Location) -> Option<Vec<Location>> {
+            let offset = self.offset_at_location(location);
+            let symbol = self.symbols.get(&offset)?;
+            self.references.get(symbol.as_str()).map(|v| v.clone())
+        }
+
+        pub fn definition(&self, location: &Location) -> Option<&Location> {
+            let offset = self.offset_at_location(location);
+            let symbol = self.symbols.get(&offset)?;
+            self.definitions.get(symbol.as_str())
+        }
     }
 
     mod tests {
+        use ebnf_parser::ParseResult;
+
+        use crate::lsp::lsp::Location;
+
+        fn parse_test_example<'src>() -> (&'src str, ParseResult<'src>) {
+            let ebnf = "very_nice_stuff = hello;\nhello = \"world\";\ncool = hello;";
+            let lexer = ebnf_parser::Lexer::new(&ebnf);
+            let parser = ebnf_parser::Parser::new(lexer);
+            let parse_result = parser.parse().expect("Should be able to parse");
+            (ebnf, parse_result)
+        }
 
         #[test]
-        fn test_add_hover() {}
+        fn test_hovers() {
+            let (ebnf, parse_result) = parse_test_example();
+            let lsp_context = super::LspContext::from_parse_results(ebnf, &parse_result);
+            let hover = lsp_context.hover.get("hello").unwrap();
+            assert_eq!(hover, "hello = \"world\";");
+            let hover = lsp_context.hover.get("cool").unwrap();
+            assert_eq!(hover, "cool = hello;");
+
+            let hover = lsp_context.hover(&Location { line: 2, col: 9 });
+            assert_eq!(hover, Some("hello = \"world\";"));
+        }
+
+        #[test]
+        fn test_refs() {
+            let (ebnf, parse_result) = parse_test_example();
+            let lsp_context = super::LspContext::from_parse_results(ebnf, &parse_result);
+            let refs = lsp_context.references.get("hello").unwrap();
+            assert_eq!(refs.len(), 3);
+            assert_eq!(refs[0], Location { line: 0, col: 18 });
+            assert_eq!(refs[1], Location { line: 1, col: 0 });
+            assert_eq!(refs[2], Location { line: 2, col: 7 });
+
+            let refs = lsp_context
+                .references(&Location { line: 2, col: 10 })
+                .expect("Should have refs");
+            assert_eq!(refs.len(), 3);
+            assert_eq!(refs[0], Location { line: 0, col: 18 });
+            assert_eq!(refs[1], Location { line: 1, col: 0 });
+            assert_eq!(refs[2], Location { line: 2, col: 7 });
+        }
+
+        #[test]
+        fn test_defs() {
+            let (ebnf, parse_result) = parse_test_example();
+            let lsp_context = super::LspContext::from_parse_results(ebnf, &parse_result);
+            let hello_def_loc = lsp_context.definitions.get("hello").unwrap();
+            assert_eq!(hello_def_loc, &Location { line: 1, col: 0 });
+            let hello_def_loc = lsp_context
+                .definition(&Location { line: 2, col: 9 })
+                .unwrap();
+            assert_eq!(hello_def_loc, &Location { line: 1, col: 0 });
+        }
     }
 }
